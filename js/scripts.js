35fc0d4426c6f10c10628f6c739e456055f8d1db
Adam Ruf <adam@adamruf.com>
2015-01-01 11:31:56 -0800

    Added easter egg animation to 404 page image

--- js/scripts.js
+++ js/scripts.js
@@ -65,6 +65,15 @@ window.onkeydown = keydown;
 // Activate tooltips
 $('.tipped').tooltip();
 
+// Animate 404 page image
+$('.js--404').click(function() {
+    $(this).addClass('animated hinge');
+
+    setTimeout(function () { 
+        $('.js--404').removeClass('animated hinge');
+    }, 2000);
+});
+
 // Typeform embed code
 (function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()
 
