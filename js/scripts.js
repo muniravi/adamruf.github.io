391fb6b421375f675e7cdec96999a67efc3e0d5e
Adam Ruf <adam@adamruf.com>
2015-05-14 23:28:13 -0700

    Removing rotating text

--- js/scripts.js
+++ js/scripts.js
@@ -67,7 +67,7 @@ $('.tipped').tooltip();
 
 // Rotate words in Hero
 var terms1 = ["teams", "startups", "creatives", "founders", "makers"];
-var terms2 = ["smiles", "insights", "innovation", "solutions", "design"];
+var terms2 = ["smiles", "insights", "design", "innovation", "solutions"];
 
 function rotateTerm1() {
   var ct = $("#rotate-1").data("term") || 0;
