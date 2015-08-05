2495fe16d8c5bf4f4581c4100392430f4f4a6f1d
Adam Ruf <adam@adamruf.com>
2015-08-05 08:08:51 -0700

    Fixing Alan Grant's email and tidied up JS file

--- portfolio/lunch-inviterator/js/random-lunch-invite.js
+++ portfolio/lunch-inviterator/js/random-lunch-invite.js
@@ -3,8 +3,8 @@ yourTeam[0]={pic:"img/kit-cloudkicker.jpg", firstname:"Kit", lastname:"Cloudkick
 yourTeam[1]={pic:"img/adam-ruf.jpg", firstname:"Adam", lastname:"Ruf", title:"Sr. UX Designer", email:"adam@adamruf.com"};
 yourTeam[2]={pic:"img/luke-skywalker.jpg", firstname:"Luke", lastname:"Skywalker", title:"Jedi", email:"luke@adamruf.com"};
 yourTeam[3]={pic:"img/sherlock-holmes.jpg", firstname:"Sherlock", lastname:"Holmes", title:"Detective", email:"sherlock@adamruf.com"};
-yourTeam[4]={pic:"img/alan-grant.jpg", firstname:"Alan", lastname:"Grant", title:"Paleontologist", email:"dennis@adamruf.com"};
-yourTeam[5]={pic:"img/ellen-ripley.jpg", firstname:"Ellen", lastname:"Ripley", title:"Warrant Officer", email:"ellen@adamruf.com"};
+yourTeam[4]={pic:"img/alan-grant.jpg", firstname:"Alan", lastname:"Grant", title:"Paleontologist", email:"agrant@adamruf.com"};
+yourTeam[5]={pic:"img/ellen-ripley.jpg", firstname:"Ellen", lastname:"Ripley", title:"Warrant Officer", email:"eripley@adamruf.com"};
 yourTeam[6]={pic:"img/ender-wiggin.jpg", firstname:"Ender", lastname:"Wiggin", title:"Speaker for the Dead", email:"ender@adamruf.com"};
 yourTeam[7]={pic:"img/daenerys-targaryen.jpg", firstname:"Daenerys", lastname:"Targaryen", title:"Mother of Dragons", email:"danaerys@adamruf.com"};
 yourTeam[8]={pic:"img/jean-luc-picard.jpg", firstname:"Jean-Luc", lastname:"Picard", title:"Captain, USS Enterprise", email:"jean-luc@adamruf.com"};
@@ -24,22 +24,15 @@ document.getElementById("name").innerHTML = "<i class=\"fa fa-user\"></i> " + ra
 document.getElementById("role").innerHTML = "<i class=\"fa fa-briefcase\"></i> " + randArray.title;
 document.getElementById("email").innerHTML = "<i class=\"fa fa-envelope\"></i> " + randArray.email;
 
-document.getElementById('template').innerHTML = "<p>Hi " + randArray.firstname + ",</p> <p>How are you doing?</p> <p>I'd like to grab lunch with you this week.</p> <p><strong>Does Thursday or Friday from 12pm - 1pm work for you?</strong></p> <p>Thanks!</p>"
+document.getElementById('template').innerHTML = "<p>Hi " + randArray.firstname + ",</p> <p>How are you doing?</p> <p>I'd like to grab lunch with you this week.</p> <p><strong>Does Thursday or Friday from 12pm - 1pm work for you?</strong></p> <p>Thanks!</p>";
 
-inviteBody = "Hi " + randArray.firstname + ",\r\n\r\n" + "How are you doing? I'd like to grab lunch with you this week.\r\n\r\n" + "Does Thursday or Friday from 12pm-1pm work for you?\r\n\r\n" + "Thanks!"
+var inviteBody = "Hi " + randArray.firstname + ",\r\n\r\n" + "How are you doing? I'd like to grab lunch with you this week.\r\n\r\n" + "Does Thursday or Friday from 12pm-1pm work for you?\r\n\r\n" + "Thanks!";
 inviteBody = encodeURIComponent(inviteBody);
 document.getElementById('draft-invite-email').innerHTML = "<a id=\"view-draft\" class=\"btn btn-md btn-info\" href=\"mailto:" + randArray.email + "?subject=INVITE:+Lunch+this+week&body=" + inviteBody + "\"><i class=\"fa fa-envelope\"></i> View Email Draft</a> <p class=\"disclaimer\"><i class=\"fa fa-lightbulb-o\"></i> If this doesn\'t launch a draft in Gmail, you need to <a href=\"https://productforums.google.com/forum/#!msg/chrome/sPhxiTQlf4s/JX9hq4JLgVYJ\">set Gmail as your default email link handler</a>.</p>";
 }
 
 $('#inviterator').click(function() {
 	randomLunchInvite();
-	// Tracking inviterator clicks
-	mixpanel.track("Generated invite");
-});
-
-$('#view-draft').click(function() {
-	// Tracking email draft views
-    mixpanel.track("Viewed email draft");
 });
 
 $('#easter-egg').click(function(){
