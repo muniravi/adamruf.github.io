fd7e6e05ee6933ef6c76eb2c8213ee5050891d72
Adam Ruf <adam@adamruf.com>
2017-08-03 21:53:43 -0700

    Updated rotating text JS and removed box-shadow from hamburger menu button

--- js/scripts.js
+++ js/scripts.js
@@ -66,8 +66,8 @@ window.onkeydown = keydown;
 $('.tipped').tooltip();
 
 // Rotate words in Hero
-var terms1 = ["codes", "cares", "leads", "mentors", "streams"];
-var terms2 = ["smiles", "impact", "innovation", "solutions", "results"];
+var terms1 = ["codes", "leads", "writes", "mentors", "streams", "cares"];
+var terms2 = ["smiles", "solutions", "impact", "kaizen", "innovation", "systems"];
 
 function rotateTerm1() {
   var ct = $("#rotate-1").data("term") || 0;
